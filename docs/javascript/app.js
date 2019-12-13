336f5575a5ed101ad6f1d8f1fb6b39e1def97911
IndigoWizard <microclubist@gmail.com>
2019-12-13 01:20:47 +0100

    Update Js+Css+Html
    updating stylying links and font
    creating gallery script
    adding photo album of Alakazam

--- docs/javascript/app.js
+++ docs/javascript/app.js
@@ -1,1 +1,21 @@
-/*Main JS file for landing page*/
+//Main JS file for landing page
+
+var pics = [
+    "/docs/src/album/Alakazam/0.jpg",
+    "/docs/src/album/Alakazam/1.png",
+    "/docs/src/album/Alakazam/2.jpg",
+    "/docs/src/album/Alakazam/3.jpg",
+    "/docs/src/album/Alakazam/4.jpg"
+];
+
+var btn = document.querySelector("button");
+var img = document.querySelector("img");
+var counter = 0;
+
+btn.addEventListener("click", function () {
+    if (counter === 5) {
+        counter = 0;
+    }
+    img.src = pics[counter];
+    counter = counter + 1;
+})
