e7121b13bf4093b949c70af3a49e230540b9ec13
IndigoWizard <microclubist@gmail.com>
2019-12-13 20:35:48 +0100

    update app.js
    fixing pics extension path

--- docs/javascript/app.js
+++ docs/javascript/app.js
@@ -5,12 +5,12 @@ var pic = []
 for (var i = 0; i < 5; i++){
     pic.push(`./src/album/${folderName.textContent}/${i}.png`);
 }
-// var pics = [
-//     "./src/album/Alakazam/0.jpg",
-//     "./src/album/Alakazam/1.png",
-//     "./src/album/Alakazam/2.jpg",
-//     "./src/album/Alakazam/3.jpg",
-//     "./src/album/Alakazam/4.jpg"
+// var pic = [
+//     "../src/album/Alakazam/0.png",
+//     "../src/album/Alakazam/1.png",
+//     "../src/album/Alakazam/2.png",
+//     "../src/album/Alakazam/3.png",
+//     "../src/album/Alakazam/4.png"
 // ];
 
 var btn = document.querySelector("button");
@@ -21,6 +21,6 @@ btn.addEventListener("click", function () {
     if (counter === 5) {
         counter = 0;
     }
-    img.src = pics[counter];
+    img.src = pic[counter];
     counter = counter + 1;
 })
