c92ab1e4c95a7bb6d3d9e277da5c3d999599f8ee
IndigoWizard <microclubist@gmail.com>
2019-12-14 01:01:39 +0100

    CSS/Index/Js fix

--- docs/javascript/app.js
+++ docs/javascript/app.js
@@ -1,17 +1,17 @@
 //Main JS file for landing page
 var folderName = document.querySelector('#title');
 folderName.textContent
-var pic = []
+/*var pic = []
 for (var i = 0; i < 5; i++){
     pic.push(`./src/album/${folderName.textContent}/${i}.png`);
-}
-// var pic = [
-//     "../src/album/Alakazam/0.png",
-//     "../src/album/Alakazam/1.png",
-//     "../src/album/Alakazam/2.png",
-//     "../src/album/Alakazam/3.png",
-//     "../src/album/Alakazam/4.png"
-// ];
+}*/
+var pic = [
+     "../src/album/Alakazam/0.png",
+     "../src/album/Alakazam/1.png",
+     "../src/album/Alakazam/2.png",
+     "../src/album/Alakazam/3.png",
+     "../src/album/Alakazam/4.png"
+];
 
 var btn = document.querySelector("button");
 var img = document.querySelector("img");
